b0b0be49a2fcc4c089ae216df95162a8f1d159fd
root <root@deepak-VirtualBox.(none)>
2013-04-03 20:17:53 +0530

    Used license instead of depreciated licence.

--- railstat/setup.py
+++ railstat/setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         "Pylons>=1.0.1rc1",
     ],
-    licence='GPLv3',
+    license='GPLv3',
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
